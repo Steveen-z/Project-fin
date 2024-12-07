ed416d700d6adac478da1933f6db36b77189fd10
Steveen-z <117107213+Steveen-z@users.noreply.github.com>
2024-12-06 20:47:25 -0600

    Update conexion.php

--- Proyecto de servidores/paginawebgym_2024/tecnologiaweb2-1/conexion.php
+++ Proyecto de servidores/paginawebgym_2024/tecnologiaweb2-1/conexion.php
@@ -1,15 +1,12 @@
-<?php
-$host = 'localhost';
-$db = 'tecnoweb';
-$user = 'root';
-$pass = '';
+$host = getenv('DB_HOST');   // Cambiar a la variable de entorno
+$port = getenv('DB_PORT');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   
-} catch(PDOException $e) {
-    echo "Error al conectar a la base de datos: " . $e->getMessage();
-   
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
 }
-?>
