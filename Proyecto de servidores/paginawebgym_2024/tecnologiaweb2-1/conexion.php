9c803e82521a3104ce2332b8f5b6f5049c0767fe
Steveen-z <117107213+Steveen-z@users.noreply.github.com>
2024-12-06 23:44:45 -0600

    Update conexion.php

--- Proyecto de servidores/paginawebgym_2024/tecnologiaweb2-1/conexion.php
+++ Proyecto de servidores/paginawebgym_2024/tecnologiaweb2-1/conexion.php
@@ -1,11 +1,12 @@
-$host = getenv('MYSQLHOST');  // mysql.railway.internal (para conexiones internas en Railway)
+<?php
+$host = getenv('MYSQLHOST');  // mysql-uuw9.railway.internal
 $port = getenv('MYSQLPORT');  // 3306
 $username = getenv('MYSQLUSER');  // root
-$password = getenv('MYSQLPASSWORD');  // La contraseña
+$password = getenv('MYSQLPASSWORD');  // BKEbKFpnpRgvQYZEiWZgNZXWbWTYDRPP
 $dbname = getenv('MYSQLDATABASE');  // railway
 
 try {
-    // Establece la conexión a la base de datos
+    // Establece la conexión a la base de datos usando las variables de entorno
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Conexión exitosa a la base de datos.";  // Puedes dejar este echo para depuración
@@ -13,3 +14,4 @@ try {
     echo "Error de conexión: " . $e->getMessage();
     exit;  // Detenemos la ejecución si no se puede conectar
 }
+?>
