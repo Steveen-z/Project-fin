ed3501991a629b5f5c06cf967065fca2b381d703
Steveen-z <117107213+Steveen-z@users.noreply.github.com>
2024-12-06 21:54:58 -0600

    Update conexion.php

--- Proyecto de servidores/paginawebgym_2024/tecnologiaweb2-1/conexion.php
+++ Proyecto de servidores/paginawebgym_2024/tecnologiaweb2-1/conexion.php
@@ -1,14 +1,15 @@
-$host = getenv('MYSQLHOST');        // Dirección del host de la base de datos (por ejemplo, autorack.proxy.rlwy.net)
-$port = getenv('MYSQLPORT');        // Puerto de la base de datos (por ejemplo, 43309)
-$username = getenv('MYSQLUSER');    // Usuario de la base de datos (por ejemplo, root)
-$password = getenv('MYSQLPASSWORD'); // Contraseña del usuario de la base de datos
-$dbname = getenv('MYSQLDATABASE');  // Nombre de la base de datos (por ejemplo, railway)
+<?php
+$host = 'localhost';
+$db = 'tecnoweb';
+$user = 'root';
+$pass = '';
 
 try {
-    // Establecer la conexión a la base de datos utilizando las variables de entorno
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa a la base de datos.";
-} catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
+   
+} catch(PDOException $e) {
+    echo "Error al conectar a la base de datos: " . $e->getMessage();
+   
 }
+?>
