24e12d9a045f16cc38d42a2538f3a5228ac82f49
Steveen-z <117107213+Steveen-z@users.noreply.github.com>
2024-12-06 21:00:00 -0600

    Update conexion.php

--- Proyecto de servidores/paginawebgym_2024/tecnologiaweb2-1/conexion.php
+++ Proyecto de servidores/paginawebgym_2024/tecnologiaweb2-1/conexion.php
@@ -1,22 +1,13 @@
-<?php
-$host = getenv('DB_HOST');     // Usar la variable de entorno para el host
-$port = getenv('DB_PORT');     // Usar la variable de entorno para el puerto
-$dbname = getenv('DB_NAME');   // Usar la variable de entorno para el nombre de la base de datos
-$username = getenv('DB_USER'); // Usar la variable de entorno para el usuario
-$password = getenv('DB_PASSWORD'); // Usar la variable de entorno para la contraseña
+$host = getenv('DB_HOST');   // Cambiar a la variable de entorno
+$port = getenv('DB_PORT');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
 
 try {
-    // DSN para la conexión PDO
-    $dsn = "mysql:host=$host;port=$port;dbname=$dbname";
-    
-    // Crear una instancia PDO
-    $pdo = new PDO($dsn, $username, $password);
-    
-    // Establecer el modo de error a excepción
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa a la base de datos.";
 } catch (PDOException $e) {
-    // Si ocurre un error, mostrarlo
     echo "Error de conexión: " . $e->getMessage();
 }
-?>
+
