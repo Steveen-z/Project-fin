5d3e02b8a4930f291409cccac4f7d4af1885f089
Steveen-z <117107213+Steveen-z@users.noreply.github.com>
2024-12-06 21:52:33 -0600

    Update registrar_usuario.php

--- Proyecto de servidores/paginawebgym_2024/tecnologiaweb2-1/registrar_usuario.php
+++ Proyecto de servidores/paginawebgym_2024/tecnologiaweb2-1/registrar_usuario.php
@@ -2,35 +2,34 @@
 require_once 'conexion.php';
 session_start();
 
-if(isset($_POST['register'])){
+if (isset($_POST['register'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     try {
         // Verifica si el usuario ya existe
         $query = "SELECT * FROM users WHERE username = :username";
-        $stmt = $pdo->prepare($query); // Cambiar $conn a $pdo
-        $stmt->execute(array(':username' => $username));
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([':username' => $username]);
         $existingUser = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if($existingUser) {
+        if ($existingUser) {
             echo "<script>alert('El usuario ya existe');</script>";
             echo "<script>window.location='registrar.php';</script>";
             exit;
         } else {
             // Inserta el nuevo usuario
             $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
-
-            $insertQuery= "INSERT INTO users (username, password)  values (:username, :password)";
-            $insertStmt= $pdo->prepare($insertQuery); // Cambiar $conn a $pdo
-            $insertStmt->execute(array(':username' => $username, ':password' => $hashedpassword));
+            $insertQuery = "INSERT INTO users (username, password) VALUES (:username, :password)";
+            $insertStmt = $pdo->prepare($insertQuery);
+            $insertStmt->execute([':username' => $username, ':password' => $hashedpassword]);
 
             echo "<script>alert('Usuario registrado exitosamente');</script>";
             echo "<script>window.location='index.php';</script>";
             exit;
         }
     } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage(); // Muestra cualquier error de PDO
+        echo "Error: " . $e->getMessage();
     }
 }
 ?>
